981da94e9c38f0edf49ccd1c27ce162819c4cfaf
vinicius brisola <vinicius.brisola@oficinamais.com.br>
2021-08-06 16:52:11 -0300

    criado rotas do salão

--- ws/index.js
+++ ws/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 require('./database');
 
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.set('port', 8000);
+
+
+
+app.use('/salao', require('./src/routes/salao.routes'))
 
 app.listen(app.get('port'), () => {
   console.log(`WS Escutando na porta ${app.get('port')}`);
