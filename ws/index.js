985cac6dac7fd12956528f0e5687e54b423b3afd
vinicius brisola <vinicius.brisola@oficinamais.com.br>
2021-08-11 13:19:04 -0300

    criando serviço na aws

--- ws/index.js
+++ ws/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const cors = require('cors');
+const cors = require('cors')
+const busboy = require('connect-busboy');
+const busboyBodyParser = require('busboy-body-perser');
+
 
 
 
@@ -10,6 +13,8 @@ require('./database');
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(busboy());
+app.use(busboyBodyParser());
 app.use(cors());
 
 app.set('port', 8000);
--- ws/src/routes/servico.routes.js
+++ ws/src/routes/servico.routes.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const Busboy = require('busboy');
+const aws = require('../services/aws');
 const Salao = require('../models/salao');
 const Servico = require('../models/servico');
 
 router.post('/', async (req, res) => {
-    try{
+    let busboy = new Busboy({ headers: req.header });
+    busboy.on('finish', async () => {
+        try{
+            let errors = [];
+            let arquivos = [];
 
-    } catch(err){
-        res.json({error: true, message: err.message});
-    }
+            if(req.files && Object.keys(req.files) > 0){
+                for(let key of Object.keys(req.files)){
+                    const file = req.files[key];
+
+                    const nameParts = file.nome.slit('.');
+                    const fileName = `${new Date().getTime()}.${
+                        nameParts[nameParts.length - 1]
+                    }`;
+                    const path = `servicos/${salaoId}/${fileName}`;
+
+                    const response = await aws.uploadToS3(file, path);
+
+                    if(response.error){
+                        errors.push({ error: true, message: response.message});
+                    }else{
+                        arquivos.push(path);
+                    }
+                }
+            }
+
+            if(errors.length > 0){
+                res.json(errors[0]);
+                return false;
+            }
+        
+        } catch(err){
+            res.json({error: true, message: err.message});
+        }
+    });
+    req.pipe(busboy);    
 });
